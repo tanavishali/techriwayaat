fbca01c281f8c1460860205dd7883ec967f3338c
tanavishali <tanawishalrai5271@gmail.com>
2026-07-16 23:31:58 +0500

    Add family option to nodemailer transport configuration

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
   },
+  family: 4,
 });
 
 module.exports = transporter;
